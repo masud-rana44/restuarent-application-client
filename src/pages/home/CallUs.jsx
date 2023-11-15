0d859520bbaed83b6cec48ecbeb8304ee3b771c9
masud-rana44 <mdmasudrana4488@gmail.com>
2023-11-15 14:35:36 +0600

    Implemented call us section

---
+++ src/pages/home/CallUs.jsx
@@ -0,0 +1,9 @@
+import { Container } from "../../components/Container"
+
+export const CallUs = () => {
+  return (
+    <Container className='bg-black text-white p-16 text-center mt-20'>
+      <p className="text-xl font-semibold font-cinzel">Call Us: +88 0192345678910</p>
+    </Container>
+  )
+}
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -4,6 +4,7 @@ import { OrderOnline } from "./OrderOnline";
 import { chefService } from "../../assets/home";
 import { Container } from "../../components/Container";
 import { FromOurMenu } from "./FromOurMenu";
+import { CallUs } from "./CallUs";
 
 const HomePage = () => {
   return <div >
@@ -13,6 +14,7 @@ const HomePage = () => {
       <Cover bgImg={chefService} bgColor='#fff' textColor='#151515' title='Bistro Boss' description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, libero accusamus laborum deserunt ratione dolor officiis praesentium! Deserunt magni aperiam dolor eius dolore at, nihil iusto ducimus incidunt quibusdam nemo.'/>
     </Container>
     <FromOurMenu/>
+    <CallUs/>
   </div>;
 };
 
