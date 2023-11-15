6fc9fd38283aba929b36c057cb4127e7d9c0c8c7
masud-rana44 <mdmasudrana4488@gmail.com>
2023-11-15 16:04:12 +0600

    Implemented testimonials

--- src/pages/home/Testimonials.jsx
+++ src/pages/home/Testimonials.jsx
@@ -1,9 +1,37 @@
+import { Swiper, SwiperSlide } from "swiper/react"
+import { useEffect, useState } from "react"
+import { Navigation } from "swiper/modules"
+
 import { SectionHeading } from "../../components/SectionHeading"
+import 'swiper/css'
+import { Container } from "../../components/Container"
+import { Quote } from 'lucide-react'
 
 export const Testimonials = () => {
+  const [reviews, setReviews] = useState([])
+
+  useEffect(() => {
+    const fetchReviews = async() => {
+      const res = await fetch('/reviews.json')
+      const data = await res.json()
+      console.log(data)
+      setReviews(data)
+    }
+    fetchReviews()
+  }, [])
+
   return (
-    <div className="mt-20">
+    <Container className="mt-20">
       <SectionHeading title='TESTIMONIALS' subtitle='---What Our Clients Say---'/>
-    </div>
+      <Swiper navigation={true} modules={[Navigation]} className='mySwiper mt-12'>
+        {reviews.map(review => <SwiperSlide key={review._id}>
+          <div className="text-center">
+            <Quote size={50} className=" fill-black mx-auto mb-6"/>
+            <p className="text-sm text-dark">{review.details}</p>
+            <h6 className="text-xl mt-2 uppercase text-yellow-500">{review.name}</h6>
+          </div>
+        </SwiperSlide>)}
+      </Swiper>
+    </Container>
   )
 }
